6b21851ed2348cdf450ad5097d74c1f91f02633b
Enoshjaguar <dasarienosh013@gmail.com>
2026-09-11 11:53:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+
+
+function clc(){
+	const age = document.getElementById('age').value
+	const name = document.getElementById('name').value
+	if(age.toString().length<=0 || name.length<=0){
+		alert("Please enter valid details")
+	}
+	else{
+		const myprom = new Promise((resolve,reject)=>{
+			if(age>18){
+				setTimeout(()=>{
+					resolve("Welcome, . You can vote.")
+				},4000)
+			}
+			else{
+				setTimeout(()=>{
+					reject("Oh sorry . You aren't old enough.")
+				},4000)
+			}
+		})
+			myprom.then((data)=>{
+	alert(data)
+}).catch((err)=>{
+	alert(err)
+})
+	}
+
+	
+}
+
